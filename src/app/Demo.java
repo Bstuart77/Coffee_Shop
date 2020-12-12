c5d2a5fc4359791dbf0f26520ae7eee661628c31
Brian <bstuart777@gmail.com>
2020-12-12 02:58:00 -0500

    added code to display the POS fxml

--- src/app/Demo.java
+++ src/app/Demo.java
@@ -1,17 +1,27 @@
 package app;
 
+import java.io.File;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Demo extends Application{
-
+public class Demo extends Application {
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-			
+	public void start(Stage pStage) throws Exception {
+		File file = new File("src/view/POS.fxml");
+		Parent root = FXMLLoader.load(file.toURI().toURL());
+		Scene scene = new Scene(root,600,454);
+		pStage.setTitle("Corner Coffee Shop");
+		pStage.setScene(scene);
+		pStage.show();
 	}
 
 }
