044db8ab4d6cf4b41376d863bf935c5c80d93326
Brian <bstuart777@gmail.com>
2020-12-13 22:04:40 -0500

    class to connect to DataBase

---
+++ src/model/ConnectionUtil.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+	public static Connection getConnection() { // checks to see if JAR JDBC is downloaded on project folder
+		try {
+			Class.forName("org.sqlite.JDBC");
+			return DriverManager.getConnection("jdbc:sqlite:data/db/CoffeeShopDB.sqlite"); // check if DB exists, if no create one
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static boolean closeConnection(Connection conn) {
+		if (conn != null) { // there IS a connection
+			try {
+				conn.close();
+				return true;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			return false;
+		}
+		return false;
+	}
+}
