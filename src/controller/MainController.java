562cd0676c55325b6dbd798c76c8923e705dda65
Brian <bstuart777@gmail.com>
2020-12-15 18:46:46 -0500

    added code to help write correct prices to textArea and price text area

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.text.DecimalFormat;
+
 import app.Demo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,24 +15,50 @@ public class MainController {
 	@FXML private Button steamedMilkButton;
 	@FXML private Button soyMilkButton;
 	@FXML public TextArea ta;
+	@FXML public TextArea priceArea;
+	private double overallPrice;
 
+	DecimalFormat df = new DecimalFormat("####.##");
 	public void writeHouseBlend(ActionEvent event) {
+		priceArea.clear();
 		ta.appendText("House Blend\t\t\t\t\s" + Demo.houseBlendPrice + "\n");
+		Demo.addTaxAndPrice = Demo.houseBlendPrice * Demo.tax;
+		Demo.price = Demo.addTaxAndPrice + Demo.houseBlendPrice;
+		overallPrice += Demo.price;
+		priceArea.setText("Testing " + df.format(overallPrice));
 	}
-
+ 
 	public void writeDarkRoast(ActionEvent event) {
+		priceArea.clear();
 		ta.appendText("Dark Roast\t\t\t\t\s" + Demo.darkRoastPrice + "\n");
+		Demo.addTaxAndPrice = Demo.darkRoastPrice * Demo.tax;
+		Demo.price = Demo.addTaxAndPrice + Demo.darkRoastPrice;
+		overallPrice += Demo.price;
+		priceArea.setText("Testing " + df.format(overallPrice));
 	}
 
 	public void writeDecaf(ActionEvent event) {
+		priceArea.clear();
 		ta.appendText("Decaf\t\t\t\t\t\s" + Demo.decafPrice + "\n");
+		Demo.addTaxAndPrice = Demo.decafPrice * Demo.tax;
+		Demo.price = Demo.addTaxAndPrice + Demo.decafPrice;
+		overallPrice += Demo.price;
+		priceArea.setText("Testing " + df.format(overallPrice));
 	}
 
 	public void writeSteamedMilk(ActionEvent event) {
 		ta.appendText("\tSteamed Milk\t\t\t\s" + Demo.steamedMilkPrice + "\n");
+		Demo.addTaxAndPrice = Demo.steamedMilkPrice * Demo.tax;
+		Demo.price = Demo.addTaxAndPrice + Demo.steamedMilkPrice;
+		overallPrice += Demo.price;
+		priceArea.setText("Testing " + df.format(overallPrice));
 	}
 
 	public void writeSoyMilk(ActionEvent event) {
 		ta.appendText("\tSoy Milk\t\t\t\t\s" + Demo.soyMilkPrice + "\n");
+		Demo.addTaxAndPrice = Demo.soyMilkPrice * Demo.tax;
+		Demo.price = Demo.addTaxAndPrice + Demo.soyMilkPrice;
+		overallPrice += Demo.price;
+		priceArea.setText("Testing " + df.format(overallPrice));
 	}
 }
