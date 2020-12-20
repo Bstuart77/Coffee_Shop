f6dfb497692a80be772e339c1a53d4df76e53241
Brian <bstuart777@gmail.com>
2020-12-20 18:09:37 -0500

    reformatted code to make a button for ever item in database

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,96 +1,120 @@
 package controller;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.DecimalFormat;
 
 import app.Demo;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import model.ConnectionUtil;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import model.ConnectionUtil;
 
 public class MainController {
-	@FXML public TextArea ta;
-	@FXML public TextArea priceArea;
+	@FXML public TextArea ta,  priceArea;
+	@FXML public HBox menuBox, condimentBox;
 	private double overallPrice;
-	private String totalOrder = "";
+	private String totalOrder;
 
+	public MainController() {
+		Platform.runLater(() -> {
+			try {
+				ta.setFont(Font.font("Monospace"));
+				priceArea.setFont(Font.font("Monospace", FontWeight.BOLD, 14));
+				Connection conn = ConnectionUtil.getConnection();
+				Statement statemnt = conn.createStatement();
+				ResultSet rs = statemnt.executeQuery("select * from item");
+				while (rs.next()) {
+					String itemName = rs.getString("name");
+					double price = rs.getDouble("price");
+					Button button = new Button(itemName);
+					button.setPrefSize(100, 50);
+					button.setOnAction(event -> {
+						addItem(itemName, price);
+					});
+					menuBox.getChildren().add(button);
+				}
+				rs = statemnt.executeQuery("select * from condiment");
+				while (rs.next()) {
+					String itemName = rs.getString("name");
+					double price = rs.getDouble("price");
+					Button button = new Button(itemName);
+					button.setPrefSize(100, 50);
+					button.setOnAction(event -> {
+						addItem(itemName, price);
+					});
+					condimentBox.getChildren().add(button);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
-	DecimalFormat df = new DecimalFormat("####.##");
-	
-	public void writeHouseBlend(ActionEvent event) {
+	public void addItem(String itemName, double price) {
 		priceArea.clear();
-		ta.appendText("House Blend\t\t\t\t\s" + Demo.houseBlendPrice + "\n");
-		Demo.addTaxAndPrice = Demo.houseBlendPrice * Demo.tax;
-		Demo.price = Demo.addTaxAndPrice + Demo.houseBlendPrice;
-		overallPrice += Demo.price;
-		priceArea.setText("Total Price: " + df.format(overallPrice));
-		totalOrder += "House Blend, ";
-		System.out.println(totalOrder);
+		String string = String.format("%-32s%1.2f\n", itemName, price);
+		ta.appendText(string);
+		double itemPrice = price * Demo.TAX + price;
+		overallPrice += itemPrice;
+
+		priceArea.setText("Total Price: \t\t\s\s" + Demo.DF.format(overallPrice));
+		totalOrder += itemName + ", ";
 	}
- 
-	public void writeDarkRoast(ActionEvent event) {
+
+	public void clear(ActionEvent event) {
+		ta.clear();
 		priceArea.clear();
-		ta.appendText("Dark Roast\t\t\t\t\s" + Demo.darkRoastPrice + "\n");
-		Demo.addTaxAndPrice = Demo.darkRoastPrice * Demo.tax;
-		Demo.price = Demo.addTaxAndPrice + Demo.darkRoastPrice;
-		overallPrice += Demo.price;
-		priceArea.setText("Total Price: " + df.format(overallPrice));
-		totalOrder += "Dark Roast, ";
-		System.out.println(totalOrder);
+		overallPrice = 0;
 	}
 
-	public void writeDecaf(ActionEvent event) {
-		priceArea.clear();
-		ta.appendText("Decaf\t\t\t\t\t\s" + Demo.decafPrice + "\n");
-		Demo.addTaxAndPrice = Demo.decafPrice * Demo.tax;
-		Demo.price = Demo.addTaxAndPrice + Demo.decafPrice;
-		overallPrice += Demo.price;
-		priceArea.setText("Total Price: " + df.format(overallPrice));
-		totalOrder += "Decaf, ";
-		System.out.println(totalOrder);
-	}
-
-	public void writeSteamedMilk(ActionEvent event) {
-		ta.appendText("\tSteamed Milk\t\t\t\s" + Demo.steamedMilkPrice + "\n");
-		Demo.addTaxAndPrice = Demo.steamedMilkPrice * Demo.tax;
-		Demo.price = Demo.addTaxAndPrice + Demo.steamedMilkPrice;
-		overallPrice += Demo.price;
-		priceArea.setText("Total Price: " + df.format(overallPrice));
-		totalOrder += "Steamed Milk, ";
-		System.out.println(totalOrder);
-	}
-
-	public void writeSoyMilk(ActionEvent event) {
-		ta.appendText("\tSoy Milk\t\t\t\t\s" + Demo.soyMilkPrice + "\n");
-		Demo.addTaxAndPrice = Demo.soyMilkPrice * Demo.tax;
-		Demo.price = Demo.addTaxAndPrice + Demo.soyMilkPrice;
-		overallPrice += Demo.price;
-		priceArea.setText("Total Price: " + df.format(overallPrice));
-		totalOrder += "Soy Milk, ";
-	}
-	
 	public void submitOrder(ActionEvent event) {
 		ta.clear();
 		priceArea.clear();
 		Alert alert = new Alert(AlertType.CONFIRMATION, "Order received!");
 		alert.showAndWait();
-		Connection conn = null;
-		conn = ConnectionUtil.getConnection();
+		writeToFile();
+
+		Connection conn = ConnectionUtil.getConnection();
 		try {
+
 			Statement statement = conn.createStatement();
-			statement.setQueryTimeout(30);	//not required, if DB doesnt respond in 30 seconds, it will quit. not required by good to have
-			statement.executeUpdate("INSERT INTO coffeeOrders(price, coffeeOrder) "
-						+ "VALUES('" +df.format(overallPrice) + "','" + totalOrder + "')");
+			statement.setQueryTimeout(30); // not required, if DB doesnt respond in 30 seconds, it will quit. not
+											// required by good to have
+			statement.execute("INSERT INTO coffeeOrders(price, coffeeOrder)" + "VALUES('" + Demo.DF.format(overallPrice)
+					+ "','" + totalOrder + "')");
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			ConnectionUtil.closeConnection(conn);
 		}
 	}
-}	
+
+	private void writeToFile() {
+		File file = new File("Sales/sales.txt");
+		try {
+			String s = "PRICE: " + Demo.DF.format(overallPrice) + " ORDER: " + totalOrder;
+			FileWriter fw = new FileWriter(file, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println(s);
+
+			pw.close();
+		} catch (Exception e) {
+
+		}
+
+	}
+}
