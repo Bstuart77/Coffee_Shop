6cabdcd9717201eb54f88654c5a7c40f20a9f60a
Brian <bstuart777@gmail.com>
2020-12-20 18:08:49 -0500

    resized stage, deleted unNeeded code

--- src/app/Demo.java
+++ src/app/Demo.java
@@ -15,44 +15,42 @@ import javafx.stage.Stage;
 import model.ConnectionUtil;
 
 public class Demo extends Application {
-	
-	final public static double houseBlendPrice = 2.00;
-	final public static double darkRoastPrice = 2.25;
-	final public static double decafPrice = 2.50;
-	final public static double steamedMilkPrice = 0.50;
-	final public static double soyMilkPrice = 0.75;
-	public static double price = 0.0;
-	final public static double tax = 0.08675;
-	public static double addTaxAndPrice = 0.0;
-	
-	DecimalFormat df = new DecimalFormat("####.##");
+	public static final double TAX = 0.08675;
+
+	public static final DecimalFormat DF = new DecimalFormat("####.##");
 
 	public static void main(String[] args) {
 		Connection conn = null;
 		conn = ConnectionUtil.getConnection();
 		try {
 			Statement statement = conn.createStatement();
-			statement.setQueryTimeout(30);	//not required, if DB doesnt respond in 30 seconds, it will quit. not required by good to have
+			statement.setQueryTimeout(30); // not required, if DB doesnt respond in 30 seconds, it will quit. not
+											// required by good to have
 			ResultSet rs = statement.executeQuery("SELECT * FROM coffeeOrders");
 
-			while(rs.next()) {
-				System.out.println("Price: " + rs.getDouble("price") + ". CoffeeOrders: " + rs.getString("coffeeOrder"));
+//			Statement s = conn.createStatement();
+//			boolean flag = s.execute("insert into item(name, price) values('Reuben Sandwhich',5.00)");
+
+			while (rs.next()) {
+				System.out
+						.println("Price: " + rs.getDouble("price") + ". CoffeeOrders: " + rs.getString("coffeeOrder"));
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			ConnectionUtil.closeConnection(conn);
 		}
 		launch(args);
+
 	}
 
 	@Override
 	public void start(Stage pStage) throws Exception {
 		File file = new File("src/view/POS.fxml");
 		Parent root = FXMLLoader.load(file.toURI().toURL());
-		Scene scene = new Scene(root,600,454);
+		Scene scene = new Scene(root, 650, 600);
 		pStage.setTitle("Corner Coffee Shop");
 		pStage.setScene(scene);
 		pStage.show();
